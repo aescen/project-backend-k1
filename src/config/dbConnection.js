40b251366ee93734c99bde9eab08e77dcc9132f0
aescen <ash.putra15@gmail.com>
2022-08-11 14:12:50 +0700

    mysql pool connection

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,12 +1,18 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
   database: process.env.DB_NAME,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  dialect: "mysql",
-  host: process.env.DB_HOST || "0.0.0.0",
-  port: process.env.DB_PORT || "3306",
+  dialect: 'mysql',
+  host: process.env.DB_HOST || '0.0.0.0',
+  port: process.env.DB_PORT || '3306',
+  pool: {
+    max: 1, // default: 5
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
 module.exports = sequelize;
